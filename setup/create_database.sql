4ac120f0b74e90de16599e5b1d01d30c42c7d03a
Dhrutabrata Biswal <dhrutabratabiswal@gmail.com>
2025-11-11 14:23:12 +0530

    some new column added

--- setup/create_database.sql
+++ setup/create_database.sql
@@ -12,3 +12,10 @@ CREATE TABLE department(
     dept_name VARCHAR(50),
     location VARCHAR(50)
 );
+
+CREATE TABLE project(
+    project_id INT PRIMARY KEY AUTO_INCREMENT,
+    project_name VARCHAR(100),
+    budget DECIMAL(10,2),
+    end_date DATE
+);
--- setup/sample_data.sql
+++ setup/sample_data.sql
@@ -1,4 +1,4 @@
-USE caompany;
+USE company;
 INSERT INTO department(dept_name,location) VALUES
 ('enggineering','Rourkela'),
 ('HR','Bangalore'),
@@ -8,3 +8,7 @@ INSERT INTO employees(name,department,salary) VALUES
 ('Dhruta','SDE', 100000 ),
 ('saloni','SDE-II',200000),
 ('Ritik','Analyst',75000);
+
+INSERT INTO project(project_id,project_name,budget) VALUES
+(1000345,'react_App',200000),
+(834789,'flutter',100000);
